26a8c0c0ee2a3642fedfbf4f76840e88cf1c068c
Randall Teves <rteves0914@gmail.com>
2021-03-28 16:01:56 -0700

    edit javascript to display name and stats

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,45 +1,91 @@
-function buildMetadata(sample) {
-    d3.csv("../Resources/Hall_of_Fame_List.csv").then((data) => {
-      //console.log(data)
-      console.log(sample);
+// Write a function that populates the dropdown menu and initializes first player data
+function init() {
+
+  // Use class "dropdown-menu" from index.html and d3 to grab the tag
+  var dropdown = d3.select(".dropdown-menu");
+
+  // Use the list of sample names to populate the select options
+  d3.csv("../Resources/player_data.csv").then((data) => {
+
+    // Create array of objects for each list item
+    var array = Object.values(data);
+    console.log(array);
+
+    // Obtain the player names from the CSV
+    var playerNames = data.map(item => item.name);
+
+    // Use forEach to populate the dropdown menu and append li tags to index.html
+    playerNames.forEach((sample) => {
+      dropdown.append("li")
+              .text(sample)
+              .classed("dropdown-item");
+    });
+
+    var firstSample = array[0].name;
+
+    buildMetadata(firstSample);
+  });
+};
+
+function buildMetadata(player) {
+
+    d3.csv("../Resources/player_data.csv").then((data) => {
+
       // Filter the data for the object with the desired sample number
-      var resultArray = data.filter(item => item.Name == sample);
-      console.log(resultArray);
+      var resultArray = data.filter(item => item.name == player);
       var result = resultArray[0];
+
+      // Create variable names to hold all the data to be displayed
+      var name = result.name;
+      var playerID = result.player_id;
+      var atBats = result.at_bats;
+      var hits = result.hits;
+      var runs = result.runs;
+      var walks = result.bases_on_balls;
+      var battingAverage = result.batting_average;
+      var doubles = result.doubles;
+      var hallOfFame = result.hall_of_fame;
+      var homeRuns = result.home_runs;
+      var RBI = result.runs_batted_in;
+      var SB = result.stolen_bases;
+      var triples = result.triples;
 
       // Use d3 to select the panel with id of `#sample-metadata`
       var namePanel = d3.select("#sample-metadata-name");
       var statPanel = d3.select("#sample-metadata-stat");
-  
-      // Use `.html("") to clear any existing metadata
+      var famePanel = d3.select("#sample-metadata-fame");
+
+      // Clear out the previous data 
       namePanel.html("");
       statPanel.html("");
-  
-      // // Use `Object.entries` to add each key and value pair to the panel
-      // // Hint: Inside the loop, you will need to use d3 to append new
-      // // tags for each key-value in the metadata.
-      // Object.entries(result).forEach(([key, value]) => {
-      //   PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-      // });
+      famePanel.html("");
+
+      namePanel.text(`Name: ${name}, Player ID: ${playerID}`);
+      statPanel.append("li")
+              .text(`At Bat: ${atBats}`)
+              .append("li")
+              .text(`Hits: ${hits}`)
+              .append("li")
+              .text(`Runs: ${runs}`)
+              .append("li")
+              .text(`Batting Average: ${battingAverage}`)
+              .append("li")
+              .text(`Walks: ${walks}`)
+              .append("li")
+              .text(`Runs Batted In: ${RBI}`)
+              .append("li")
+              .text(`Doubles: ${doubles}`)
+              .append("li")
+              .text(`Triples: ${triples}`)
+              .append("li")
+              .text(`Home Runs: ${homeRuns}`)
+              .append("li")
+              .text(`Stolen Bases: ${SB}`);
+      famePanel.text(hallOfFame);
     });
 }
 
-function init() {
-    // Grab a reference to the dropdown select element
-    var dropdown = d3.select(".dropdown-menu");
-  
-    // Use the list of sample names to populate the select options
-    d3.csv("../Resources/Hall_of_Fame_List.csv").then((data) => {
-      var playerNames = data.map(item => item.Name)
-      console.log(playerNames)
-  
-      playerNames.forEach((sample) => {
-        dropdown.append("li")
-                .text(sample)
-                .classed("dropdown-item");
-      });
-    });
-  };
+
   
 // function optionChanged(newSample) {
 //     // Fetch new data each time a new sample is selected
@@ -48,4 +94,6 @@ function init() {
   
 // Initialize the dashboard
 init();
-d3.selectAll("#makeChange").on("click", buildMetadata(this));
+
+// Event listener for changing names on dropdown
+d3.selectAll("#navbarDropdownMenuLink").on("click", buildMetadata(this.value));
